6e11ec4da8c7aa255cebd09244aa2b4df64e6e7c
KaueAlves09 <kaueyuukialves9@gmail.com>
2025-11-21 18:59:39 -0400

    Main finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
 import model.DAO.DBconfig;
-import model.DAO.MercadinhoDAO;
-import model.DAO.PessoaDAO;
-import model.DAO.ProdutoDAO;
-import model.Mercadinho;
-import model.Pessoa;
-import model.Produto;
+import view.MainFrame;
+import view.Menu;
+import controller.MenuController;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main{
-    public static void main(String[] args){
-        try(Connection conexao = DBconfig.getConnection()){
-            System.out.println("Conectado ao banco de dados " + conexao.isValid(5));
+public class Main {
 
-            MercadinhoDAO mercadinhoDAO = new MercadinhoDAO(conexao);
-            Mercadinho mercadinho = new Mercadinho(1, 100, 1500.20);
-            mercadinhoDAO.create(mercadinho);
+    public static void main(String[] args) {
 
-        } catch(SQLException e){
-            System.out.println("Erro ao conectar ao banco de dados " + e);
+        // Iniciar conexão (opcional, apenas teste)
+        try (Connection conexao = DBconfig.getConnection()) {
+            System.out.println("Conectado ao banco de dados: " + conexao.isValid(5));
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco: " + e.getMessage());
         }
 
+        // INICIAR INTERFACE GRÁFICA
+        javax.swing.SwingUtilities.invokeLater(() -> {
 
+            MainFrame frame = new MainFrame();
+            Menu menu = new Menu();
+
+            // Controller que conecta botões do menu → troca de telas
+            new MenuController(menu, frame);
+
+            frame.trocarTela(menu);
+            frame.setVisible(true);
+        });
     }
 }
